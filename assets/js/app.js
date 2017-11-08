433d7930ee4614e49597928fa498f3cf8a3705f5
J0ssy <j.g.basterrica@gmail.com>
2017-11-08 04:58:34 -0300

    base todavía

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var boton = document.getElementById('savebtn');
+
+
+boton.addEventListener('click', function() {
+
+	var textInput = document.getElementById('addlistimpunt').value
+		//Limpiar el textarea
+		textInput = '';
+
+}); 
